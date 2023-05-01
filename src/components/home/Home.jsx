cf1b8e5059205dd3275bd7d3106f923d5b609c6f
rohith <rohith.goli@anywhere.co>
2023-05-01 12:31:26 +0530

    configuring relative imports for simplification

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,39 @@
+import Animation from 'images/animation.jpeg';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Profile Component</h2>
+            <img src={Animation} alt="Animation" class="img"/>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Maecenas tincidunt metus a venenatis ullamcorper. 
+                Morbi bibendum nibh vel ex sollicitudin, ut ullamcorper leo suscipit. 
+                Pellentesque eget molestie augue, sed aliquet ex. Praesent sed justo metus. 
+                Etiam viverra eleifend neque quis posuere. 
+                Integer non tortor lacinia dolor condimentum tempus. 
+                Vestibulum vitae iaculis enim.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Maecenas tincidunt metus a venenatis ullamcorper. 
+                Morbi bibendum nibh vel ex sollicitudin, ut ullamcorper leo suscipit. 
+                Pellentesque eget molestie augue, sed aliquet ex. Praesent sed justo metus. 
+                Etiam viverra eleifend neque quis posuere. 
+                Integer non tortor lacinia dolor condimentum tempus. 
+                Vestibulum vitae iaculis enim.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Maecenas tincidunt metus a venenatis ullamcorper. 
+                Morbi bibendum nibh vel ex sollicitudin, ut ullamcorper leo suscipit. 
+                Pellentesque eget molestie augue, sed aliquet ex. Praesent sed justo metus. 
+                Etiam viverra eleifend neque quis posuere. 
+                Integer non tortor lacinia dolor condimentum tempus. 
+                Vestibulum vitae iaculis enim.
+            </p>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,39 @@
+import DeliciousFood from 'images/delicious_food.jpg';
+
+const Profile = () => {
+    return (
+        <div>
+            <h2>Profile Component</h2>
+            <img src={DeliciousFood} alt="Delicious Food" class="img"/>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Maecenas tincidunt metus a venenatis ullamcorper. 
+                Morbi bibendum nibh vel ex sollicitudin, ut ullamcorper leo suscipit. 
+                Pellentesque eget molestie augue, sed aliquet ex. Praesent sed justo metus. 
+                Etiam viverra eleifend neque quis posuere. 
+                Integer non tortor lacinia dolor condimentum tempus. 
+                Vestibulum vitae iaculis enim.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Maecenas tincidunt metus a venenatis ullamcorper. 
+                Morbi bibendum nibh vel ex sollicitudin, ut ullamcorper leo suscipit. 
+                Pellentesque eget molestie augue, sed aliquet ex. Praesent sed justo metus. 
+                Etiam viverra eleifend neque quis posuere. 
+                Integer non tortor lacinia dolor condimentum tempus. 
+                Vestibulum vitae iaculis enim.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Maecenas tincidunt metus a venenatis ullamcorper. 
+                Morbi bibendum nibh vel ex sollicitudin, ut ullamcorper leo suscipit. 
+                Pellentesque eget molestie augue, sed aliquet ex. Praesent sed justo metus. 
+                Etiam viverra eleifend neque quis posuere. 
+                Integer non tortor lacinia dolor condimentum tempus. 
+                Vestibulum vitae iaculis enim.
+            </p>
+        </div>
+    )
+}
+
+export default Profile
